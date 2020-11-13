ac24aee0cf484ab19377caa27c95878d82e931f2
40478642 <40478642@live.napier.ac.uk>
2020-11-13 13:05:55 +0630

    Update World Population.puml and add getter setter functions to java file

--- src/main/java/com/napier/gp7/Country.java
+++ src/main/java/com/napier/gp7/Country.java
@@ -1,10 +1,81 @@
 package com.napier.gp7;
 
 public class Country {
-    public String code;
-    public String name;
-    public String continent;
-    public String region;
-    public int population;
-    public int capital;
+    //variable1
+    //Create variable for code
+    private String code;
+    //getter for code
+    public String getCode() {
+        return code;
+    }
+    // Setter for code
+    public void setCode(String newcode) {
+        this.code = newcode;
+    }
+    //End
+
+    //variable 2
+    //Create variable for name
+    private String name;
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+    // Setter for name
+    public void setName(String newName) {
+        this.name = newName;
+    }
+    //end
+
+    //variable3
+    //Create variable for continent
+    private String continent;
+    // Getter for continent
+    public String getContinent() {
+        return continent;
+    }
+    // Setter for continent
+    public void setContinent(String newCon) {
+        this.continent = newCon;
+    }
+    //end
+
+    //variable4
+    //Create variable for region
+    private String region;
+    // Getter for region
+    public String getRegion() {
+        return region;
+    }
+    // Setter for region
+    public void setRegion(String newReg) {
+        this.region = newReg;
+    }
+    //end
+
+    //variable5
+    //Create variable for population
+    private int population;
+    // Getter for population
+    public int getPopulation() {
+        return population;
+    }
+    // Setter for continent
+    public void setPopulation(int newPop) {
+        this.population = newPop;
+    }
+    //end
+
+    //variable6
+    //Create variable for capital
+    private int capital;
+    // Getter for capital
+    public int getCapital() {
+        return capital;
+    }
+    // Setter for capital
+    public void setCapital(int newPop) {
+        this.capital = newPop;
+    }
+    //end
 }
