d84671d1052f61f989694c5b050a41a85fa9c1d0
40478642 <40478642@live.napier.ac.uk>
2020-11-24 13:42:48 +0630

    Implement the left user stories

--- src/main/java/com/napier/gp7/GP7.java
+++ src/main/java/com/napier/gp7/GP7.java
@@ -1,7 +1,9 @@
 package com.napier.gp7;
 //import sql Package
+import java.math.BigInteger;
 import java.sql.*;
 import java.util.ArrayList;
+import java.math.*;
 
 public class GP7
 {
@@ -116,6 +118,15 @@ public class GP7
         ArrayList<City> popcitylistdistrict = a.pop_city_district();
         a.print_populationcity_District(popcitylistdistrict);
 
+        ArrayList<Country> peopleList = a.peopleListCountry();
+        a.printPeopleListCountry(peopleList);
+
+        //Print Population of CapitalCity in the world
+        System.out.println("\n");
+        System.out.println("Population of CapitalCity in the world");
+        ArrayList<City> capitalCtyWorld = a.capitalCityWorld();
+        a.printCapitalcityWorld(capitalCtyWorld);
+
         // Disconnect from database
         a.disconnect();
     }
@@ -140,14 +151,14 @@ public class GP7
             System.exit(-1);
         }
 
-        int retries = 10;
+        int retries = 100;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
             try
             {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
+                Thread.sleep(3000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://" + s + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
@@ -938,6 +949,109 @@ public class GP7
         }
     }
     public void print_populationcity_District(ArrayList<City> City1)
+    {
+        //Check null
+        if(City1 == null) {
+            System.out.println("No Population City List information in District");
+            return;
+        }
+        // Print header
+        System.out.println(String.format("%-10s %-30s %-45s %-55s", "Population", "City Name", "Country Name","district" ));
+        // Loop over all employees in the list
+        for (City emp : City1) {
+            if (emp == null)
+                continue;
+            String emp_string =
+                    String.format("%-10s %-30s %-45s %-55s",
+                            emp.getPopulation(), emp.getName(), emp.getCountry(),emp.getDistrict());
+            System.out.println(emp_string);
+        }
+    }
+    //End
+
+    //Function 17
+    public ArrayList<Country> peopleListCountry(){
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String ctypeoplelist = "Select city.Population,city.Name,country.Population, country.Code from city,country where city.CountryCode = country.Code and country.Name = 'Myanmar'";
+            // Execute SQL statement
+            ResultSet rset1 = stmt.executeQuery(ctypeoplelist);
+            // Extract employee information
+            ArrayList<Country> City = new ArrayList<Country>();
+            while (rset1.next())
+            {
+                Country city = new Country();
+                city.setPopulation(rset1.getInt("Population"));
+                city.setPopulation(rset1.getInt("Population"));
+                city.setName(rset1.getString("Name"));
+                City.add(city);
+            }
+            return City;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Fail to print Country list");
+            return null;
+        }
+    }
+    public void printPeopleListCountry(ArrayList<Country> City1)
+    {
+        //Check null
+        if(City1 == null) {
+            System.out.println("No Population City List information in District");
+            return;
+        }
+        // Print header
+        //System.out.println(String.format("%-10s", "Population"));
+        // Loop over all employees in the list
+        for (Country emp : City1) {
+            if (emp == null)
+                continue;
+            String emp_string = ("Population in Myanmar :"+ emp.getPopulation() +".");
+            System.out.println(emp_string);
+        }
+        for (Country emp : City1) {
+            if (emp == null)
+                continue;
+            String emp_string = ("Population in Myanmar :"+ emp.getPopulation() +".");
+            System.out.println(emp_string);
+        }
+    }
+    //End
+
+    //Function 17
+    public ArrayList<City> capitalCityWorld(){
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String citylist = "Select city.Population,city.ID, city.Name, city.District, city.CountryCode, country.Capital from city,country where city.ID = country.Capital Order By city.Population DESC ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(citylist);
+            // Extract employee information
+            ArrayList<City> City = new ArrayList<City>();
+            while (rset.next())
+            {
+                City city = new City();
+//                city.setCode(rset.getString("Code"));
+                city.setCountry(rset.getString("CountryCode"));
+                city.setName(rset.getString("Name"));
+//                city.setRegion(rset.getString("Region"));
+                city.setPopulation(rset.getInt("Population"));
+                city.setDistrict(rset.getString("District"));
+                City.add(city);
+            }
+            return City;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Fail to print Country list");
+            return null;
+        }
+    }
+    public void printCapitalcityWorld(ArrayList<City> City1)
     {
         //Check null
         if(City1 == null) {
